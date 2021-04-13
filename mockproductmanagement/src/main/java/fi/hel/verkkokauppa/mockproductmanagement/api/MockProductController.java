c435a7beae6ff118d54cdd664b5985f074dc6543
Henri Leisma <henri.leisma@ambientia.fi>
2021-04-13 09:56:33 +0300

    KYV-17 mock backend for simple product data

---
+++ mockproductmanagement/src/main/java/fi/hel/verkkokauppa/mockproductmanagement/api/MockProductController.java
@@ -0,0 +1,37 @@
+package fi.hel.verkkokauppa.mockproductmanagement.api;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MockProductController {
+
+    private Map<String, MockProduct> mockProductsAP = new HashMap<String, MockProduct>();
+    private Map<String, MockProduct> mockProductsTV = new HashMap<String, MockProduct>();
+
+    {
+        mockProductsAP.put("1234", new MockProduct("1234", "asukaspysakointi tuote 1", "kuvaus", "100"));
+        mockProductsAP.put("12345", new MockProduct("12345", "asukaspysakointi tuote 2", "kuvaus", "200"));
+        mockProductsAP.put("123456", new MockProduct("123456", "asukaspysakointi tuote 3", "kuvaus", "300"));
+
+        mockProductsTV.put("1234", new MockProduct("1234", "tilavaraus tuote 1", "kuvaus", "100"));
+        mockProductsTV.put("12345", new MockProduct("12345", "tilavaraus tuote 2", "kuvaus", "200"));
+        mockProductsTV.put("123456", new MockProduct("123456", "tilavaraus tuote 3", "kuvaus", "300"));
+    }
+
+    @GetMapping("/mockproductmanagement/asukaspysakointi/get")
+    public MockProduct getMockProductAP(@RequestParam(value = "productId") String productId) {
+        return mockProductsAP.get(productId);
+    }
+
+    @GetMapping("/mockproductmanagement/tilavaraus/get")
+    public MockProduct getMockProductTV(@RequestParam(value = "productId") String productId) {
+        return mockProductsTV.get(productId);
+    }
+
+}
---
+++ mockproductmanagement/src/test/java/fi/hel/verkkokauppa/mockproductmanagement/MockproductmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.hel.verkkokauppa.mockproductmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockproductmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
