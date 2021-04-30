fac08adfb6ccb5f910e9d9dc7c63ccfefd423e2e
Henri Leisma <henri.leisma@ambientia.fi>
2021-04-30 16:32:00 +0300

    KYV-72 product caching persistence, field type

--- productapi/src/main/java/fi/hel/verkkokauppa/product/model/Product.java
+++ productapi/src/main/java/fi/hel/verkkokauppa/product/model/Product.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonRawValue;
 import org.json.JSONObject;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
@@ -15,10 +16,10 @@ public class Product {
     String id;
     @Field(type = FieldType.Text)    
     String name;
-    @Field(type = FieldType.Object)    
+    @Transient
     @JsonRawValue
     JSONObject mapping;
-    @Field(type = FieldType.Object)    
+    @Transient
     @JsonRawValue
     JSONObject original;
 
