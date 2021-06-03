6c8677810b02c367abdac9b8702550d48d58b0b9
Janne Huovinen <janne.huovinen@ambientia.fi>
2021-06-03 14:43:11 +0300

    KYV-168 - Refactor product mapping to use common module

--- productmapping/src/main/java/fi/hel/verkkokauppa/productmapping/service/ProductMappingService.java
+++ productmapping/src/main/java/fi/hel/verkkokauppa/productmapping/service/ProductMappingService.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import fi.hel.verkkokauppa.productmapping.model.ProductMapping;
-import fi.hel.verkkokauppa.utils.UUIDGenerator;
+import fi.hel.verkkokauppa.common.util.UUIDGenerator;
 
 @Component
 public class ProductMappingService {
