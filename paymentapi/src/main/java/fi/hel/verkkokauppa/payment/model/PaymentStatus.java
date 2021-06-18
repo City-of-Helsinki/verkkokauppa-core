3e9309464b22b511922f5f468f37cffd98f14252
Henri Leisma <henri.leisma@ambientia.fi>
2021-06-18 16:41:52 +0300

    KYV-178 payment fields and construction

---
+++ paymentapi/src/main/java/fi/hel/verkkokauppa/payment/model/PaymentStatus.java
@@ -0,0 +1,5 @@
+package fi.hel.verkkokauppa.payment.model;
+
+public class PaymentStatus {
+    public static String CREATED = "payment_created";
+}
--- paymentapi/src/main/java/fi/hel/verkkokauppa/payment/service/OnlinePaymentService.java
+++ paymentapi/src/main/java/fi/hel/verkkokauppa/payment/service/OnlinePaymentService.java
@@ -1,15 +1,44 @@
 package fi.hel.verkkokauppa.payment.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import fi.hel.verkkokauppa.common.util.UUIDGenerator;
 import fi.hel.verkkokauppa.payment.api.OrderPaymentDto;
+import fi.hel.verkkokauppa.payment.model.Payment;
 
 @Component
 public class OnlinePaymentService {
 
+    private Logger log = LoggerFactory.getLogger(OnlinePaymentService.class);
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+
     public String createFromOrder(OrderPaymentDto dto) {
-        //TODO a redirect url containing id of created Payment
-        return null;
+
+        // TODO get common payment configuration from configuration api
+
+        // construct a Payment entity
+        Payment payment = createPayment(dto);
+        String paymentId = payment.getPaymentId();
+
+        // TODO a redirect url containing id of created Payment
+        return "https://localhost/?paymentId=" + paymentId;
+    }
+
+    private Payment createPayment(OrderPaymentDto dto) {
+        String namespace = dto.getNamespace();
+        String paymentId = UUIDGenerator.generateType3UUIDString(namespace, dto.getOrderId());
+        Payment payment = new Payment(paymentId, namespace, dto.getOrderId(), dto.getPaymentType(), dto.getSum(), dto.getDescription());
+
+        paymentRepository.save(payment);
+        log.debug("created payment for namespace: " + namespace + " with paymentId: " + paymentId);
+
+        return payment;
     }
     
 }
