57271d663e7ded30b6831eeac859a9a28cc394b8
Henri Leisma <henri.leisma@ambientia.fi>
2021-06-29 16:38:34 +0300

    KYV-180 read common configuration to payment api from service configuration api

--- paymentapi/src/main/java/fi/hel/verkkokauppa/payment/service/OnlinePaymentService.java
+++ paymentapi/src/main/java/fi/hel/verkkokauppa/payment/service/OnlinePaymentService.java
@@ -1,9 +1,11 @@
 package fi.hel.verkkokauppa.payment.service;
 
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import fi.hel.verkkokauppa.common.util.UUIDGenerator;
 import fi.hel.verkkokauppa.payment.api.OrderPaymentDto;
@@ -17,10 +19,22 @@ public class OnlinePaymentService {
     @Autowired
     private PaymentRepository paymentRepository;
 
+    @Autowired
+    private ServiceConfigurationClient serviceConfigurationClient;
+
 
     public String createFromOrder(OrderPaymentDto dto) {
 
-        // TODO get common payment configuration from configuration api
+        // get common payment configuration from configuration api
+        WebClient client = serviceConfigurationClient.getClient();
+        String namespace = dto.getNamespace();
+        JSONObject namespaceServiceConfiguration = serviceConfigurationClient.getAllServiceConfiguration(client, namespace);
+
+        // refer to ServiceConfigurationKeys
+        String payment_return_url = (String) namespaceServiceConfiguration.get("payment_return_url");
+        log.debug("namespace: " + namespace + " payment_return_url: " + payment_return_url);
+
+        // TODO use the common payment configuration values
 
         // construct a Payment entity
         Payment payment = createPayment(dto);
