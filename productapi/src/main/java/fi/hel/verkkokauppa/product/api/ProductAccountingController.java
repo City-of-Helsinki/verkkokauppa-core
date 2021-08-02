3781423a03cc181bf8078e1058e20631d5bc591c
Eero Hakio <eero.hakio@ambientia.fi>
2021-08-02 11:42:40 +0300

    KYV-121: Remove duplicate dependencies, return accounting from product accounting controller

--- productapi/src/main/java/fi/hel/verkkokauppa/product/api/ProductAccountingController.java
+++ productapi/src/main/java/fi/hel/verkkokauppa/product/api/ProductAccountingController.java
@@ -2,8 +2,10 @@ package fi.hel.verkkokauppa.product.api;
 
 import fi.hel.verkkokauppa.product.constants.ApiUrls;
 import fi.hel.verkkokauppa.product.dto.ProductAccountingDto;
+import fi.hel.verkkokauppa.product.model.ProductAccounting;
 import fi.hel.verkkokauppa.product.service.ProductAccountingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,11 +15,14 @@ public class ProductAccountingController {
     private ProductAccountingService productAccountingService;
 
     @PostMapping(ApiUrls.PRODUCT_ROOT + "/{productId}/" + ApiUrls.ACCOUNTING)
-    public void createProductAccounting(@PathVariable final String productId,
+    public ResponseEntity<ProductAccounting> createProductAccounting(@PathVariable final String productId,
                                         @RequestBody ProductAccountingDto productAccountingDto)
     {
         productAccountingDto.setProductId(productId);
 
-        productAccountingService.createProductAccounting(productAccountingDto);
+        final ProductAccounting productAccounting = productAccountingService.createProductAccounting(productAccountingDto);
+        ResponseEntity<ProductAccounting> response = ResponseEntity.ok().body(productAccounting);
+
+        return response;
     }
 }
--- productapi/src/main/java/fi/hel/verkkokauppa/product/service/ProductAccountingService.java
+++ productapi/src/main/java/fi/hel/verkkokauppa/product/service/ProductAccountingService.java
@@ -2,6 +2,7 @@ package fi.hel.verkkokauppa.product.service;
 
 import fi.hel.verkkokauppa.product.ProductAccountingTransformer;
 import fi.hel.verkkokauppa.product.dto.ProductAccountingDto;
+import fi.hel.verkkokauppa.product.model.Product;
 import fi.hel.verkkokauppa.product.model.ProductAccounting;
 import fi.hel.verkkokauppa.product.repository.ProductAccountingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +14,9 @@ public class ProductAccountingService {
     @Autowired
     private ProductAccountingRepository productAccountingRepository;
 
-    public void createProductAccounting(ProductAccountingDto productAccountingDto) {
+    public ProductAccounting createProductAccounting(ProductAccountingDto productAccountingDto) {
         ProductAccounting productAccountingEntity = new ProductAccountingTransformer().transform(productAccountingDto);
         this.productAccountingRepository.save(productAccountingEntity);
+        return productAccountingEntity;
     }
 }
