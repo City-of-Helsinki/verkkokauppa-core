05884a51860cb7dda6d04469726c307c19c7a315
Karoliina Elo <karoliina.elo@ambientia.fi>
2021-08-24 09:42:52 +0300

    KYV-172 Added basic error handling to cart api

--- cartapi/src/main/java/fi/hel/verkkokauppa/cart/api/CartController.java
+++ cartapi/src/main/java/fi/hel/verkkokauppa/cart/api/CartController.java
@@ -1,81 +1,137 @@
 package fi.hel.verkkokauppa.cart.api;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import fi.hel.verkkokauppa.cart.model.Cart;
 import fi.hel.verkkokauppa.cart.model.CartItem;
 import fi.hel.verkkokauppa.cart.service.CartItemService;
 import fi.hel.verkkokauppa.cart.service.CartService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CartController {
+	private Logger log = LoggerFactory.getLogger(CartController.class);
 
-    @Autowired
-    private CartService cartService;
+	@Autowired
+	private CartService cartService;
 
-    @Autowired
-    private CartItemService cartItemService;
+	@Autowired
+	private CartItemService cartItemService;
 
 
-    @GetMapping("/cart/create")
-	public Cart createCart(@RequestParam(value = "namespace") String namespace, 
-            @RequestParam(value = "user") String user) {
-		return cartService.createByParams(namespace, user);
+	@GetMapping("/cart/create")
+	public ResponseEntity<Cart> createCart(@RequestParam(value = "namespace") String namespace,
+										   @RequestParam(value = "user") String user) {
+		try {
+			return ResponseEntity.ok().body(cartService.createByParams(namespace, user));
+		} catch (Exception e) {
+			log.error("creating cart failed", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
-    @GetMapping("/cart/get")
-	public Cart getCart(@RequestParam(value = "cartId") String cartId) {
-		return cartService.findById(cartId);
+	@GetMapping("/cart/get")
+	public ResponseEntity<Cart> getCart(@RequestParam(value = "cartId") String cartId) {
+		try {
+			Cart cart = cartService.findById(cartId);
+
+			if (cart == null) {
+				return ResponseEntity.notFound().build();
+			}
+
+			return ResponseEntity.ok().body(cart);
+		} catch (Exception e) {
+			log.error("getting cart failed, cartId: " + cartId, e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
-    @GetMapping("/cart/getByNames")
-	public Cart getCartByNames(@RequestParam(value = "namespace") String namespace, 
-            @RequestParam(value = "user") String user) {
-		return cartService.findByNamespaceAndUser(namespace, user);
+	@GetMapping("/cart/getByNames")
+	public ResponseEntity<Cart> getCartByNames(@RequestParam(value = "namespace") String namespace,
+											   @RequestParam(value = "user") String user) {
+		try {
+			Cart cart = cartService.findByNamespaceAndUser(namespace, user);
+
+			if (cart == null) {
+				return ResponseEntity.notFound().build();
+			}
+
+			return ResponseEntity.ok().body(cart);
+		} catch (Exception e) {
+			log.error("getting cart failed, namespace: " + namespace + "user: " + user, e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
-    @GetMapping("/cart/getCartWithItems")
-	public CartDto getCartWithItems(@RequestParam(value = "cartId") String cartId) {
-		Cart cart = cartService.findById(cartId);
-		List<CartItem> items = cartItemService.findByCartId(cartId);
+	@GetMapping("/cart/getCartWithItems")
+	public ResponseEntity<CartDto> getCartWithItems(@RequestParam(value = "cartId") String cartId) {
+		try {
+			Cart cart = cartService.findById(cartId);
+			List<CartItem> items = cartItemService.findByCartId(cartId);
 
-        return new CartDto(cart, items);
+			return ResponseEntity.ok().body(new CartDto(cart, items));
+		} catch (Exception e) {
+			log.error("getting cart with items failed, cartId: " + cartId, e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
 	@GetMapping("/cart/addItem")
-	public CartDto addItem(@RequestParam(value = "cartId") String cartId, @RequestParam(value = "productId") String productId, 
-			@RequestParam(value = "quantity", required = false, defaultValue = "1") Integer quantity) {
-		cartItemService.addItem(cartId, productId, quantity);
-		return getCartWithItems(cartId);
+	public ResponseEntity<CartDto> addItem(@RequestParam(value = "cartId") String cartId, @RequestParam(value = "productId") String productId,
+										   @RequestParam(value = "quantity", required = false, defaultValue = "1") Integer quantity) {
+		try {
+			cartItemService.addItem(cartId, productId, quantity);
+			return getCartWithItems(cartId);
+		} catch (Exception e) {
+			log.error("adding cart item failed, cartId: " + cartId + ", productId: " + productId, e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
 	@GetMapping("/cart/removeItem")
-	public CartDto removeItem(@RequestParam(value = "cartId") String cartId, @RequestParam(value = "productId") String productId, 
-			@RequestParam(value = "quantity", required = false, defaultValue = "0") Integer quantity) {
-		cartItemService.removeItem(cartId, productId, quantity);
-		return getCartWithItems(cartId);
+	public ResponseEntity<CartDto> removeItem(@RequestParam(value = "cartId") String cartId, @RequestParam(value = "productId") String productId,
+											  @RequestParam(value = "quantity", required = false, defaultValue = "0") Integer quantity) {
+		try {
+			cartItemService.removeItem(cartId, productId, quantity);
+			return getCartWithItems(cartId);
+		} catch (Exception e) {
+			log.error("removing cart item failed, cartId: " + cartId + ", productId: " + productId, e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
 	@GetMapping("/cart/editItem")
-	public CartDto editItem(@RequestParam(value = "cartId") String cartId, 
-		@RequestParam(value = "productId") String productId, @RequestParam(value = "quantity") Integer quantity) {
-		cartItemService.editItemQuantity(cartId, productId, quantity);
-		return getCartWithItems(cartId);
+	public ResponseEntity<CartDto> editItem(@RequestParam(value = "cartId") String cartId,
+											@RequestParam(value = "productId") String productId, @RequestParam(value = "quantity") Integer quantity) {
+		try {
+			cartItemService.editItemQuantity(cartId, productId, quantity);
+			return getCartWithItems(cartId);
+		} catch (Exception e) {
+			log.error("editing cart item quantity failed, cartId: " + cartId + ", productId: " + productId, e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
 	@GetMapping("/cart/clear")
-	public Cart clearCart(@RequestParam(value = "namespace") String namespace, @RequestParam(value = "user") String user) {
-		Cart cart = getCartByNames(namespace, user);
-		if (cart != null) {
-			cartItemService.removeAllByCartId(cart.getCartId());
-		}
+	public ResponseEntity<Cart> clearCart(@RequestParam(value = "namespace") String namespace, @RequestParam(value = "user") String user) {
+		try {
+			Cart cart = getCartByNames(namespace, user).getBody();
+			if (cart != null) {
+				cartItemService.removeAllByCartId(cart.getCartId());
+			}
 
-		return createCart(namespace, user);
+			return createCart(namespace, user);
+		} catch (Exception e) {
+			log.error("clearing cart failed, namespace: " + namespace + "user: " + user, e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
 }
