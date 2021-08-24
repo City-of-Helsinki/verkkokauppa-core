fca4cc0a02909ace64b595bdfb49107626a1e6ff
Karoliina Elo <karoliina.elo@ambientia.fi>
2021-08-24 09:47:00 +0300

    KYV-170 Added basic error handling to product apis

--- productapi/src/main/java/fi/hel/verkkokauppa/product/api/ProductController.java
+++ productapi/src/main/java/fi/hel/verkkokauppa/product/api/ProductController.java
@@ -1,7 +1,11 @@
 package fi.hel.verkkokauppa.product.api;
 
 import fi.hel.verkkokauppa.product.constants.ApiUrls;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,19 +16,42 @@ import fi.hel.verkkokauppa.product.service.ProductService;
 
 @RestController
 public class ProductController {
+	private Logger log = LoggerFactory.getLogger(ProductController.class);
 
-    @Autowired
-    private ProductService service;
+	@Autowired
+	private ProductService service;
 
 
 	@GetMapping(ApiUrls.PRODUCT_ROOT + "/get")
-	public Product getProduct(@RequestParam(value = "productId") String productId) {
-		return service.findById(productId);
+	public ResponseEntity<Product> getProduct(@RequestParam(value = "productId") String productId) {
+		try {
+			Product product = service.findById(productId);
+
+			if (product == null) {
+				return ResponseEntity.notFound().build();
+			}
+
+			return ResponseEntity.ok().body(product);
+		} catch (Exception e) {
+			log.error("getting product failed, productId: " + productId, e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
 	@GetMapping(ApiUrls.PRODUCT_ROOT + "/getFromBackend")
-	public Product getFromBackend(@RequestParam(value = "productId") String productId) {
-		return service.getFromBackend(productId);
+	public ResponseEntity<Product> getFromBackend(@RequestParam(value = "productId") String productId) {
+		try {
+			Product product = service.getFromBackend(productId);
+
+			if (product == null) {
+				return ResponseEntity.notFound().build();
+			}
+
+			return ResponseEntity.ok().body(product);
+		} catch (Exception e) {
+			log.error("getting product from backend failed, productId: " + productId, e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
 }
--- productmapping/src/main/java/fi/hel/verkkokauppa/productmapping/api/ProductMappingController.java
+++ productmapping/src/main/java/fi/hel/verkkokauppa/productmapping/api/ProductMappingController.java
@@ -2,7 +2,11 @@ package fi.hel.verkkokauppa.productmapping.api;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +16,7 @@ import fi.hel.verkkokauppa.productmapping.service.ProductMappingService;
 
 @RestController
 public class ProductMappingController {
+	private Logger log = LoggerFactory.getLogger(ProductMappingController.class);
 
     @Autowired
     private ProductMappingService service;
@@ -23,8 +28,19 @@ public class ProductMappingController {
      * @return ProductMapping with common productId as a UUID string and original backend identifiers
      */
 	@GetMapping("/productmapping/get")
-	public ProductMapping getProductMapping(@RequestParam(value = "productId") String productId) {
-		return service.findById(productId);
+	public ResponseEntity<ProductMapping> getProductMapping(@RequestParam(value = "productId") String productId) {
+		try {
+			ProductMapping productMapping = service.findById(productId);
+
+			if (productMapping == null) {
+				return ResponseEntity.notFound().build();
+			}
+
+			return ResponseEntity.ok().body(productMapping);
+		} catch (Exception e) {
+			log.error("getting product mapping failed, productId: " + productId, e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
     /**
@@ -36,12 +52,22 @@ public class ProductMappingController {
      * @return ProductMapping with common productId as a UUID string and original backend identifiers
      */
     @GetMapping("/productmapping/create")
-	public ProductMapping createProductMapping(@RequestParam(value = "namespace") String namespace, @RequestParam(value = "namespaceEntityId") String namespaceEntityId) {
-		return service.createByParams(namespace, namespaceEntityId);
+	public ResponseEntity<ProductMapping> createProductMapping(@RequestParam(value = "namespace") String namespace, @RequestParam(value = "namespaceEntityId") String namespaceEntityId) {
+		try {
+			return ResponseEntity.ok().body(service.createByParams(namespace, namespaceEntityId));
+		} catch (Exception e) {
+			log.error("creating product mapping failed", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
     @GetMapping("/productmapping/initializetestdata")
-	public List<ProductMapping> initializeTestData() {
-        return service.initializeTestData();
+	public ResponseEntity<List<ProductMapping>> initializeTestData() {
+        try {
+			return ResponseEntity.ok().body(service.initializeTestData());
+		} catch (Exception e) {
+			log.error("initializing product mapping test data failed", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 }
