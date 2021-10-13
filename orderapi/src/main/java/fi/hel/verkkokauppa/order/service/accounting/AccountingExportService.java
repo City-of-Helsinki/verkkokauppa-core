48fc845634ee9a7054096181f4cc30d97bb90448
Karoliina Elo <karoliina.elo@ambientia.fi>
2021-10-13 11:31:12 +0300

    KYV-89 Small fix to log entry

--- orderapi/src/main/java/fi/hel/verkkokauppa/order/service/accounting/AccountingExportService.java
+++ orderapi/src/main/java/fi/hel/verkkokauppa/order/service/accounting/AccountingExportService.java
@@ -115,7 +115,7 @@ public class AccountingExportService {
     private ChannelSftp setupJsch() throws JSchException {
         JSch jsch = new JSch();
 
-        log.debug("Connecting to server: [{}], with username", sftpServerUsername);
+        log.debug("Connecting to server with username [{}]", sftpServerUsername);
         Session jschSession = jsch.getSession(sftpServerUsername, sftpServerUrl);
         jschSession.setPassword(sftpServerPassword);
 
