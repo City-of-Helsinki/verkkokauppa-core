ecf27a53efe9ce1a159b49fce2985f894d955a76
Henri Leisma <henri.leisma@ambientia.fi>
2021-11-05 10:11:24 +0200

    KYV-347 kafka client scram authentication

--- eventsapi/src/main/java/fi/hel/verkkokauppa/events/configuration/KafkaConsumerConfig.java
+++ eventsapi/src/main/java/fi/hel/verkkokauppa/events/configuration/KafkaConsumerConfig.java
@@ -45,9 +45,13 @@ public class KafkaConsumerConfig {
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
         if (kafkaClientAuthenticationEnabled) {
-            props.put("security.protocol", "PLAINTEXT");
-            props.put("sasl.mechanism", "PLAIN");
-            props.put("sasl.jaas.config", PlainLoginModule.class.getName() + " required username=\"" + kafkaUser + "\" password=\"" + kafkaPassword + "\";");
+            props.put("security.protocol", "SASL_PLAINTEXT");
+            props.put(SaslConfigs.SASL_MECHANISM, "SCRAM-SHA-512");
+            props.put(SaslConfigs.SASL_JAAS_CONFIG, "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"" + kafkaUser + "\" password=\"" + kafkaPassword + "\";");
+
+            //props.put("security.protocol", "PLAINTEXT");
+            //props.put("sasl.mechanism", "PLAIN");
+            //props.put("sasl.jaas.config", PlainLoginModule.class.getName() + " required username=\"" + kafkaUser + "\" password=\"" + kafkaPassword + "\";");
         }
 
         return props;
