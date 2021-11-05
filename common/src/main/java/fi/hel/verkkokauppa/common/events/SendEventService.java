a7d592097d08ce66294959ddd839e2fcfba14a08
Henri Leisma <henri.leisma@ambientia.fi>
2021-11-05 14:35:19 +0200

    KYV-347 event message json conversion

--- common/src/main/java/fi/hel/verkkokauppa/common/events/SendEventService.java
+++ common/src/main/java/fi/hel/verkkokauppa/common/events/SendEventService.java
@@ -1,6 +1,9 @@
 package fi.hel.verkkokauppa.common.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import fi.hel.verkkokauppa.common.events.message.EventMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
@@ -9,6 +12,11 @@ import org.springframework.stereotype.Component;
 public class SendEventService {
     private KafkaTemplate<String, String> kafkaTemplate;
     private KafkaTemplate<String, EventMessage> eventMessageKafkaTemplate;
+
+    private Logger log = LoggerFactory.getLogger(SendEventService.class);
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Autowired
     SendEventService(KafkaTemplate<String, String> kafkaTemplate, KafkaTemplate<String, EventMessage> eventMessageKafkaTemplate) {
@@ -21,7 +29,20 @@ public class SendEventService {
     }
 
     public void sendEventMessage(String topicName, EventMessage message) {
-        eventMessageKafkaTemplate.send(topicName, message);
+        try {
+            log.debug("sending to topic: " + topicName);
+
+            //format payload, message to json string conversion
+            String body = objectMapper.writeValueAsString(message);
+            log.debug("message converted to JSON: " + body);
+
+            // TODO send once, either as string or eventmessage
+            kafkaTemplate.send(topicName, body);
+            //eventMessageKafkaTemplate.send(topicName, message);
+
+        } catch (Exception e) {
+            log.error("failed to send event message to topic: " + topicName, e);
+        }
     }
 
 }
