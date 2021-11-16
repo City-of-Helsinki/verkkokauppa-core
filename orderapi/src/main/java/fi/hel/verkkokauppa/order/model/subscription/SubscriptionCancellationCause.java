83f0f2b1f94d6159be9e44852f5e5cba426ff656
Henri Leisma <henri.leisma@ambientia.fi>
2021-11-16 14:58:47 +0200

    KYV-402 subscription cancellation timestamp and cause, triggering subscription cancelled event

---
+++ orderapi/src/main/java/fi/hel/verkkokauppa/order/model/subscription/SubscriptionCancellationCause.java
@@ -0,0 +1,7 @@
+package fi.hel.verkkokauppa.order.model.subscription;
+
+public class SubscriptionCancellationCause {
+    public static String CUSTOMER_CANCELLED = "CUSTOMER_CANCELLED";
+    public static String EXPIRED = "EXPIRED";
+
+}
--- orderapi/src/main/java/fi/hel/verkkokauppa/order/service/subscription/CancelSubscriptionCommand.java
+++ orderapi/src/main/java/fi/hel/verkkokauppa/order/service/subscription/CancelSubscriptionCommand.java
@@ -1,20 +1,34 @@
 package fi.hel.verkkokauppa.order.service.subscription;
 
+import fi.hel.verkkokauppa.common.events.EventType;
+import fi.hel.verkkokauppa.common.events.SendEventService;
+import fi.hel.verkkokauppa.common.events.TopicName;
+import fi.hel.verkkokauppa.common.events.message.SubscriptionMessage;
+import fi.hel.verkkokauppa.common.util.DateTimeUtil;
+import fi.hel.verkkokauppa.order.api.SubscriptionController;
 import fi.hel.verkkokauppa.order.model.subscription.Subscription;
 import fi.hel.verkkokauppa.order.model.subscription.SubscriptionStatus;
 import fi.hel.verkkokauppa.order.repository.jpa.SubscriptionRepository;
 import fi.hel.verkkokauppa.shared.exception.EntityNotFoundException;
 import fi.hel.verkkokauppa.shared.service.BaseServiceOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
 public class CancelSubscriptionCommand extends BaseServiceOperation {
+
+	private Logger log = LoggerFactory.getLogger(CancelSubscriptionCommand.class);
+
+	@Autowired
+	private SendEventService sendEventService;
 
 	private final SubscriptionRepository repository;
 	private final GetSubscriptionQuery getSubscriptionQuery;
@@ -30,35 +44,32 @@ public class CancelSubscriptionCommand extends BaseServiceOperation {
 		this.getSubscriptionQuery = getSubscriptionQuery;
 	}
 
-	public void cancel(String id, String userId) {
+	public Subscription cancel(String id, String userId, String cause) {
 		final Subscription subscription = getSubscriptionQuery.findByIdValidateByUser(id, userId);
+		LocalDateTime cancelledAt = DateTimeUtil.getFormattedDateTime();
+		doCancel(subscription, cancelledAt);
+		triggerSubscriptionCancelledEvent(subscription, cause, cancelledAt);
 
-		checkAbilityToCancelSubscription(subscription);
-		doCancel(subscription);
+		return subscription;
 	}
 
-	private Subscription initSubscription(String id) {
-		assertRequiredParameterPresent(id, "id");
-
-		final Optional<Subscription> result = repository.findById(id);
-		if (result.isEmpty()) {
-			throw new EntityNotFoundException(repository.getEntityType(), id);
-		}
-
-		return result.get();
-	}
-
-	private void checkAbilityToCancelSubscription(Subscription subscription) {
-		final Errors errors = createErrors(subscription.getId());
-
-		// TODO: implement needed validation logic and call it here
-
-		assertNoErrors(errors);
-	}
-
-	private void doCancel(Subscription subscription) {
-		subscription.setStatus(SubscriptionStatus.CANCELLED); // TODO: implement status logic separately?
+	private void doCancel(Subscription subscription, LocalDateTime cancelledAt) {
+		subscription.setStatus(SubscriptionStatus.CANCELLED);
+		subscription.setCancelledAt(cancelledAt);
 
 		repository.save(subscription);
 	}
+
+	public void triggerSubscriptionCancelledEvent(Subscription subscription, String cause, LocalDateTime cancelledAt) {
+		SubscriptionMessage subscriptionMessage = SubscriptionMessage.builder()
+				.eventType(EventType.SUBSCRIPTION_CANCELLED)
+				.namespace(subscription.getNamespace())
+				.subscriptionId(subscription.getId())
+				.timestamp(DateTimeUtil.getFormattedDateTime(cancelledAt))
+				.cancellationCause(cause)
+				.build();
+		sendEventService.sendEventMessage(TopicName.SUBSCRIPTIONS, subscriptionMessage);
+		log.debug("triggered event SUBSCRIPTION_CANCELLED for subscriptionId: " + subscription.getId());
+	}
+
 }
