2f33a7091d823ce9aac23222ebb624918f302e35
severikupari1 <severikupari1@gmail.com>
2021-12-10 10:17:07 +0200

    Another test fix

--- orderapi/src/test/java/fi/hel/verkkokauppa/order/test/utils/KafkaTestConsumer.java
+++ orderapi/src/test/java/fi/hel/verkkokauppa/order/test/utils/KafkaTestConsumer.java
@@ -6,10 +6,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.test.context.TestPropertySource;
 
 import java.util.concurrent.CountDownLatch;
 
 @Component
+@TestPropertySource(properties = {
+        "test.topic= ",
+        "test.groupId= ",
+        "kafka.client.authentication.enabled=false"
+})
 public class KafkaTestConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
@@ -22,7 +28,7 @@ public class KafkaTestConsumer {
 //            groupId="payments",
 //            containerFactory="paymentsKafkaListenerContainerFactory")
 
-    @KafkaListener(topics = "${test.topic#null}", groupId="${test.groupId#null}")
+    @KafkaListener(topics = "${test.topic}", groupId="${test.groupId}")
     public void receive(ConsumerRecord<?, ?> consumerRecord) {
         LOGGER.info("received payload='{}'", consumerRecord.toString());
         setPayload(consumerRecord);
