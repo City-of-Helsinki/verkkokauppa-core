a5436dc5b4c8fad4976e245ab6067db43f826b8a
severikupari1 <severikupari1@gmail.com>
2022-02-02 16:55:12 +0200

    KYV-418 Added validations

--- common/src/main/java/fi/hel/verkkokauppa/common/history/dto/HistoryDto.java
+++ common/src/main/java/fi/hel/verkkokauppa/common/history/dto/HistoryDto.java
@@ -3,6 +3,7 @@ package fi.hel.verkkokauppa.common.history.dto;
 import fi.hel.verkkokauppa.common.contracts.history.History;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
 
 import java.time.LocalDateTime;
 
@@ -10,15 +11,18 @@ import java.time.LocalDateTime;
 @Builder
 public class HistoryDto implements History {
     String historyId;
+    @NonNull
     String entityId;
     String user;
 
     @Builder.Default
     Boolean isVisible = false;
-
+    @NonNull
     String entityType;
     LocalDateTime createdAt;
+    @NonNull
     String namespace;
+    @NonNull
     String eventType;
     String payload;
     String description;
--- historyapi/src/main/java/fi/hel/verkkokauppa/history/api/HistoryController.java
+++ historyapi/src/main/java/fi/hel/verkkokauppa/history/api/HistoryController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +23,7 @@ public class HistoryController {
 
 
     @PostMapping(value = "/history/create", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<HistoryDto> createHistory(@RequestBody HistoryDto history) {
+    public ResponseEntity<HistoryDto> createHistory(@Validated @RequestBody HistoryDto history) {
         try {
             HistoryModel createdModel = historyService.saveHistory(history);
             HistoryDto returningDto = historyService.mapToDto(createdModel);
