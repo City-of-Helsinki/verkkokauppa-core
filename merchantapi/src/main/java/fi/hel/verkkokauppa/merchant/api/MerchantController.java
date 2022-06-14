dffa3d6515d8cffa89915dd57f23d38c54d97cbf
severikupari1 <severikupari1@gmail.com>
2022-06-14 13:50:15 +0300

    [KYV-603] Added functional tests for endpoints

--- merchantapi/src/main/java/fi/hel/verkkokauppa/merchant/api/MerchantController.java
+++ merchantapi/src/main/java/fi/hel/verkkokauppa/merchant/api/MerchantController.java
@@ -1,5 +1,6 @@
 package fi.hel.verkkokauppa.merchant.api;
 
+import fi.hel.verkkokauppa.common.configuration.ServiceConfigurationKeys;
 import fi.hel.verkkokauppa.common.error.CommonApiException;
 import fi.hel.verkkokauppa.common.error.Error;
 import fi.hel.verkkokauppa.merchant.api.dto.MerchantDto;
@@ -10,11 +11,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Validated
@@ -54,5 +55,30 @@ public class MerchantController {
                     new Error("failed-to-upsert-merchant", "failed to upsert merchant, merchantId:" + merchantDto.getMerchantId())
             );
         }
+    }
+
+    @GetMapping("/merchant/keys")
+    public ResponseEntity<List<String>> getKeys() {
+        return ResponseEntity.ok(ServiceConfigurationKeys.getAllConfigurationKeys());
+    }
+
+    @GetMapping("/merchant/getValue")
+    public ResponseEntity<String> getValue(
+            @RequestParam(value = "merchantId") String merchantId,
+            @RequestParam(value = "namespace") String namespace,
+            @RequestParam(value = "key") String key
+    ) {
+        return ResponseEntity.ok(
+                merchantService.getConfigurationValueByMerchantIdAndNamespaceAndKey(merchantId, namespace, key)
+        );
+    }
+
+    @GetMapping("/merchant/list-by-namespace")
+    public ResponseEntity<List<MerchantDto>> getMerchantsByNamespace(@RequestParam(value = "namespace") String namespace) {
+        List<MerchantDto> merchantDtos = merchantService.findAllByNamespace(namespace);
+
+        return ResponseEntity.ok(
+                merchantDtos
+        );
     }
 }
--- merchantapi/src/test/java/fi/hel/verkkokauppa/merchant/api/MerchantControllerTest.java
+++ merchantapi/src/test/java/fi/hel/verkkokauppa/merchant/api/MerchantControllerTest.java
@@ -2,11 +2,14 @@ package fi.hel.verkkokauppa.merchant.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fi.hel.verkkokauppa.common.error.CommonApiException;
+import fi.hel.verkkokauppa.common.util.UUIDGenerator;
 import fi.hel.verkkokauppa.merchant.api.dto.MerchantDto;
 import fi.hel.verkkokauppa.merchant.model.ConfigurationModel;
 import fi.hel.verkkokauppa.merchant.model.LocaleModel;
+import fi.hel.verkkokauppa.merchant.repository.MerchantRepository;
 import fi.hel.verkkokauppa.merchant.testing.annotations.RunIfProfile;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +19,8 @@ import org.springframework.http.ResponseEntity;
 
 import javax.validation.ConstraintViolationException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,11 +32,26 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 @SpringBootTest
 @Slf4j
 public class MerchantControllerTest {
+    private ArrayList<String> toBeDeleted = new ArrayList<>();
     @Autowired
     private ObjectMapper objectMapper;
 
     @Autowired
     private MerchantController merchantController;
+    @Autowired
+    private MerchantRepository merchantRepository;
+
+    @AfterEach
+    void tearDown() {
+        try {
+            toBeDeleted.forEach(s -> merchantRepository.deleteById(s));
+            // Clear list because all merchants deleted
+            toBeDeleted = new ArrayList<>();
+        } catch (Exception e) {
+            log.info("delete error {}", e.toString());
+        }
+
+    }
 
     /**
      * It tests the upsert merchant endpoint.
@@ -79,8 +99,7 @@ public class MerchantControllerTest {
             Assertions.assertEquals(response.get().getStatusCode(), HttpStatus.BAD_REQUEST);
             Assertions.assertNull(response.get().getBody());
         });
-        Assertions.assertEquals(exception.getMessage(),"upsertMerchant.merchantDto.namespace: namespace cant be blank");
-
+        Assertions.assertEquals(exception.getMessage(), "upsertMerchant.merchantDto.namespace: namespace cant be blank");
 
     }
 
@@ -109,7 +128,9 @@ public class MerchantControllerTest {
 
         MerchantDto responseMerchantDto = response.getBody();
         Assertions.assertNotNull(responseMerchantDto);
-        Assertions.assertNotNull(responseMerchantDto.getMerchantId());
+        String merchantId = responseMerchantDto.getMerchantId();
+        toBeDeleted.add(merchantId);
+        Assertions.assertNotNull(merchantId);
         Assertions.assertNotNull(responseMerchantDto.getCreatedAt());
         Assertions.assertNotNull(responseMerchantDto.getConfigurations());
         Assertions.assertEquals(1, responseMerchantDto.getConfigurations().size());
@@ -120,6 +141,151 @@ public class MerchantControllerTest {
         Assertions.assertEquals("test-fi", responseMerchantDto.getConfigurations().get(0).getLocale().getFi());
         Assertions.assertEquals("test-sv", responseMerchantDto.getConfigurations().get(0).getLocale().getSv());
         Assertions.assertEquals("test-en", responseMerchantDto.getConfigurations().get(0).getLocale().getEn());
+    }
+
+
+    @Test
+    @RunIfProfile(profile = "local")
+    public void getValueReturnsCorrectValueAndStatus200() throws Exception {
+        MerchantDto merchantDto = new MerchantDto();
+        String namespace = "test-namespace" + UUIDGenerator.generateType4UUID();
+        merchantDto.setNamespace(namespace);
+
+        ConfigurationModel configurationModel = new ConfigurationModel();
+
+        String requestKey = "test-key";
+        String requestValue = "test-value";
+
+        configurationModel.setKey(requestKey);
+        configurationModel.setValue(requestValue);
+
+        ArrayList<ConfigurationModel> configurationModels = new ArrayList<>() {{
+            add(configurationModel);
+        }};
+
+
+        merchantDto.setConfigurations(configurationModels);
+
+        ResponseEntity<MerchantDto> response = merchantController.upsertMerchant(merchantDto);
+
+        MerchantDto responseMerchantDto = response.getBody();
+        Assertions.assertNotNull(responseMerchantDto);
+        String merchantId = responseMerchantDto.getMerchantId();
+        toBeDeleted.add(merchantId);
+        Assertions.assertNotNull(merchantId);
+        Assertions.assertEquals(1, responseMerchantDto.getConfigurations().size());
+        Assertions.assertEquals(namespace, responseMerchantDto.getNamespace());
+
+        ConfigurationModel responseConfiguration = responseMerchantDto.getConfigurations().get(0);
+
+        String responseKey = responseConfiguration.getKey();
+        String responseValue = responseConfiguration.getValue();
+
+        Assertions.assertEquals(requestKey, responseKey);
+        Assertions.assertEquals(requestValue, responseValue);
+
+        ResponseEntity<String> getValueResponse = merchantController.getValue(
+                responseMerchantDto.getMerchantId(),
+                namespace,
+                requestKey
+        );
+        log.info("getValueResponse : {}", getValueResponse);
+        Assertions.assertEquals(requestValue, getValueResponse.getBody());
+
+        ResponseEntity<String> getValueResponseShouldBeNull = merchantController.getValue(
+                responseMerchantDto.getMerchantId(),
+                namespace,
+                "KEY_SHOULD_NOT_BE_FOUND"
+        );
+
+        log.info("getValueResponseShouldBeNull : {}", getValueResponseShouldBeNull);
+        Assertions.assertNull(getValueResponseShouldBeNull.getBody());
+
+    }
+
+    @Test
+    @RunIfProfile(profile = "local")
+    public void getMerchantsByNamespace() throws Exception {
+        MerchantDto merchantDto = new MerchantDto();
+        String namespace = "test-namespace" + UUIDGenerator.generateType4UUID();
+        merchantDto.setNamespace(namespace);
+        ConfigurationModel configurationModel = new ConfigurationModel();
+
+        String requestKey = "test-key";
+        String requestValue = "test-value";
+
+        configurationModel.setKey(requestKey);
+        configurationModel.setValue(requestValue);
+        ArrayList<ConfigurationModel> configurationModels = new ArrayList<>() {{
+            add(configurationModel);
+        }};
+
+        merchantDto.setConfigurations(configurationModels);
+        // Create 2 merchants
+        ResponseEntity<MerchantDto> response = merchantController.upsertMerchant(merchantDto);
+        ResponseEntity<MerchantDto> response2 = merchantController.upsertMerchant(merchantDto);
+
+        MerchantDto responseMerchantDto = response.getBody();
+        MerchantDto responseMerchantDto2 = response2.getBody();
+        Assertions.assertNotNull(responseMerchantDto);
+        String merchantId = responseMerchantDto.getMerchantId();
+        String merchantId2 = Objects.requireNonNull(responseMerchantDto2).getMerchantId();
+        toBeDeleted.add(merchantId);
+        toBeDeleted.add(merchantId2);
+        Assertions.assertNotNull(merchantId);
+        Assertions.assertNotNull(merchantId);
+        Assertions.assertEquals(1, responseMerchantDto.getConfigurations().size());
+        Assertions.assertEquals(namespace, responseMerchantDto.getNamespace());
+
+        ConfigurationModel responseConfiguration = responseMerchantDto.getConfigurations().get(0);
+
+        String responseKey = responseConfiguration.getKey();
+        String responseValue = responseConfiguration.getValue();
+
+        Assertions.assertEquals(requestKey, responseKey);
+        Assertions.assertEquals(requestValue, responseValue);
+
+        ResponseEntity<List<MerchantDto>> merchantsByNamespace = merchantController.getMerchantsByNamespace(namespace);
+
+        Assertions.assertEquals(merchantsByNamespace.getStatusCode(), HttpStatus.OK);
+        Assertions.assertEquals(2, Objects.requireNonNull(merchantsByNamespace.getBody()).size());
+
+        ResponseEntity<List<MerchantDto>> merchantsByNamespaceShouldBeEmptyList = merchantController.getMerchantsByNamespace(UUIDGenerator.generateType4UUID().toString());
+        Assertions.assertEquals(0, Objects.requireNonNull(merchantsByNamespaceShouldBeEmptyList.getBody()).size());
+
+    }
+
+    @Test
+    @RunIfProfile(profile = "local")
+    public void merchantGetKeys() {
+        ResponseEntity<List<String>> responseMerchantKeys = merchantController.getKeys();
+        List<String> allKeys = new ArrayList<>(new ArrayList<>() {{
+            add("merchantName");
+            add("merchantStreet");
+            add("merchantZip");
+            add("merchantCity");
+            add("merchantEmail");
+            add("merchantPhone");
+            add("merchantUrl");
+            add("merchantTermsOfServiceUrl");
+            add("merchantPaymentWebhookUrl");
+            add("orderRightOfPurchaseIsActive");
+            add("orderRightOfPurchaseUrl");
+            add("merchantOrderWebhookUrl");
+            add("merchantSubscriptionWebhookUrl");
+            add("subscriptionPriceUrl");
+            add("payment_api_version");
+            add("payment_api_key");
+            add("payment_currency");
+            add("payment_type");
+            add("payment_register_card_token");
+            add("payment_return_url");
+            add("payment_notification_url");
+            add("payment_language");
+            add("payment_submerchant_id");
+            add("payment_cp");
+        }});
+        Assertions.assertEquals(allKeys, responseMerchantKeys.getBody());
     }
 
 }
