aef85ee4661d54fcb4f3cb423ce2527e10d274ac
martin.lehtomaa <martin.lehtomaa@ambientia.fi>
2022-07-06 10:08:01 +0300

    [KYV-620] Removed unnecessary class PaymentFieldMessage, modified email template to have a general information slot and did some general review fixes.

--- orderapi/src/main/java/fi/hel/verkkokauppa/order/listeners/DeadLetterQueueListener.java
+++ orderapi/src/main/java/fi/hel/verkkokauppa/order/listeners/DeadLetterQueueListener.java
@@ -3,8 +3,8 @@ package fi.hel.verkkokauppa.order.listeners;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fi.hel.verkkokauppa.common.configuration.QueueConfigurations;
+import fi.hel.verkkokauppa.common.configuration.ServiceUrls;
 import fi.hel.verkkokauppa.common.events.EventType;
-import fi.hel.verkkokauppa.common.events.message.PaymentFailedMessage;
 import fi.hel.verkkokauppa.common.events.message.PaymentMessage;
 import fi.hel.verkkokauppa.common.queue.service.SendNotificationService;
 import fi.hel.verkkokauppa.common.rest.RestServiceClient;
@@ -34,8 +34,8 @@ public class DeadLetterQueueListener {
     @Value("${payment.failed.notification.email:donotreply.checkout@hel.fi}")
     private String paymentFailedNotificationEmail;
 
-    @Value("${message.service.url}")
-    private String messageServiceUrl;
+    @Autowired
+    private ServiceUrls serviceUrls;
 
     @Autowired
     private ObjectMapper mapper;
@@ -77,7 +77,7 @@ public class DeadLetterQueueListener {
 
             logMessageData(message);
 
-            if (message.getEventType().equals(EventType.PAYMENT_PAID)) {
+            if (message.getEventType() != null && message.getEventType().equals(EventType.PAYMENT_PAID)) {
                 paymentFailedAction(message);
             }
         } catch(JsonProcessingException jsonProcessingException) {
@@ -87,36 +87,29 @@ public class DeadLetterQueueListener {
     }
 
     private void paymentFailedAction(PaymentMessage message) {
-        PaymentFailedMessage paymentFailedMessage = PaymentFailedMessage.builder()
-                .eventType(EventType.PAYMENT_FAILED)
-                .paymentId(message.getPaymentId())
-                .eventTimestamp(LocalDateTime.now().toString())
-                .namespace(message.getNamespace())
-                .build();
-
-        sendNotificationToEmail(paymentFailedMessage);
-        sendNotificationService.sendToQueue(paymentFailedMessage, queueConfigurations.getPaymentFailedToProcessQueue());
+        sendNotificationToEmail(message);
+        sendNotificationService.sendToQueue(message, queueConfigurations.getPaymentFailedToProcessQueue());
     }
 
-    private void sendNotificationToEmail(PaymentFailedMessage paymentFailedMessage) {
+    private void sendNotificationToEmail(PaymentMessage paymentMessage) {
         JSONObject msgJson = new JSONObject();
-        msgJson.put("id", paymentFailedMessage.getPaymentId());
+        msgJson.put("id", paymentMessage.getPaymentId());
         msgJson.put("receiver", paymentFailedNotificationEmail);
-        msgJson.put("header", "DLQ queue alert - " + EventType.PAYMENT_FAILED);
+        msgJson.put("header", "DLQ queue alert - " + EventType.PAYMENT_PAID);
         try {
             String html = Files.readString(Paths.get(ClassLoader.getSystemResource(EMAIL_TEMPLATE_PATH).toURI()));
-            html = html.replace("#EVENT_TYPE#", paymentFailedMessage.getEventType());
-            html = html.replace("#PAYMENT_ID#", paymentFailedMessage.getPaymentId());
-            html = html.replace("#NAMESPACE#", paymentFailedMessage.getNamespace());
-            html = html.replace("#EVENT_PAYLOAD#", mapper.writeValueAsString(paymentFailedMessage));
+            html = html.replace("#EVENT_TYPE#", paymentMessage.getEventType());
+            html = html.replace("#GENERAL_INFORMATION#", "<p>" + paymentMessage.getPaymentId() + "</p>" );
+            html = html.replace("#NAMESPACE#", paymentMessage.getNamespace());
+            html = html.replace("#EVENT_PAYLOAD#", mapper.writeValueAsString(paymentMessage));
 
             msgJson.put("body", html);
         } catch (IOException | URISyntaxException e) {
             throw new RuntimeException(e);
         }
 
-        log.info("Payment with id {} failed. Sending email notification to {}", paymentFailedMessage.getPaymentId(), paymentFailedNotificationEmail);
-        restServiceClient.makePostCall(messageServiceUrl + "send/email", msgJson.toString());
+        log.info("Payment with id {} failed. Sending email notification to {}", paymentMessage.getPaymentId(), paymentFailedNotificationEmail);
+        restServiceClient.makePostCall(serviceUrls.getMessageServiceUrl() + "/message/send/email", msgJson.toString());
     }
 
     private PaymentMessage getPaymentMessageFromTextMessage(TextMessage textMessage) throws JMSException, JsonProcessingException {
@@ -127,11 +120,10 @@ public class DeadLetterQueueListener {
     }
 
     /**
-     * Logs eventType, orderId and paymentId from PaymentMessage
+     * Logs the DLQ message
      */
     private void logMessageData(PaymentMessage message) throws JsonProcessingException {
-        log.info(mapper.writeValueAsString(message));
-        log.info("Message eventType: {}, orderId: {}, paymentId: {}", message.getEventType(), message.getOrderId(), message.getPaymentId());
+        log.info("DLQ-Message: " + mapper.writeValueAsString(message));
     }
 
 }
