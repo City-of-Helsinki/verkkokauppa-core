e1736f570fe0f309ee999c9fbd06294fc61ac7e2
martin.lehtomaa <martin.lehtomaa@ambientia.fi>
2022-08-22 15:25:55 +0300

    [KYV-682] Added test property source and profile annotation to test that needs only to be run on local env.

--- orderapi/src/test/java/fi/hel/verkkokauppa/order/service/invoice/InvoiceServiceTest.java
+++ orderapi/src/test/java/fi/hel/verkkokauppa/order/service/invoice/InvoiceServiceTest.java
@@ -12,8 +12,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.activemq.broker-url=failover:(tcp://localhost:61616)?startupMaxReconnectAttempts=1")
+@Profile("local")
 class InvoiceServiceTest extends TestUtils {
 
     @Autowired
