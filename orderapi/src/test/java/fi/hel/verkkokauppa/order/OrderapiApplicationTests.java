4b9331e0f5de692a7a9fd1c7ba184f46f5ecfa8a
martin.lehtomaa <martin.lehtomaa@ambientia.fi>
2022-08-26 11:33:00 +0300

    [KYV-682] Reverted exclusion of ActiveMQ and Kafka auto-config in Application test.

--- orderapi/src/test/java/fi/hel/verkkokauppa/order/OrderapiApplicationTests.java
+++ orderapi/src/test/java/fi/hel/verkkokauppa/order/OrderapiApplicationTests.java
@@ -4,10 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.elasticsearch.ReactiveElasticsearchRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.elasticsearch.ReactiveElasticsearchRestClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @EnableAutoConfiguration(exclude = {
+		ActiveMQAutoConfiguration.class,
+		KafkaAutoConfiguration.class,
 		ReactiveElasticsearchRepositoriesAutoConfiguration.class,
 		ReactiveElasticsearchRestClientAutoConfiguration.class
 })
