db5f9fdd320c3e7488c8d1ea7db21cfeef5e418a
severikupari1 <severikupari1@gmail.com>
2023-03-17 14:48:41 +0200

    [KYV-853] Added new admin endpoint to fetch refund payments

---
+++ paymentapi/src/main/java/fi/hel/verkkokauppa/payment/api/PaytrailRefundPaymentAdminController.java
@@ -0,0 +1,46 @@
+package fi.hel.verkkokauppa.payment.api;
+
+import fi.hel.verkkokauppa.common.error.CommonApiException;
+import fi.hel.verkkokauppa.common.error.Error;
+import fi.hel.verkkokauppa.payment.model.refund.RefundPayment;
+import fi.hel.verkkokauppa.payment.service.refund.PaytrailRefundPaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class PaytrailRefundPaymentAdminController {
+
+    private final PaytrailRefundPaymentService refundPaymentService;
+
+    @Autowired
+    public PaytrailRefundPaymentAdminController(PaytrailRefundPaymentService onlinePaymentService) {
+        this.refundPaymentService = onlinePaymentService;
+    }
+
+    @GetMapping(value = "/refund-admin/refund-payment/get", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<RefundPayment> getRefundPayment(@RequestParam(value = "orderId") String orderId) {
+        try {
+            RefundPayment refundPaymentForOrder = refundPaymentService.getRefundPaymentForOrder(orderId);
+            if (refundPaymentForOrder == null) {
+                throw new Exception("No refund payments found with given orderId. refundPaymentForOrder == null");
+            }
+            return ResponseEntity.status(HttpStatus.OK).body(refundPaymentForOrder);
+        } catch (CommonApiException cae) {
+            throw cae;
+        } catch (Exception e) {
+            log.error("getting refund payment failed, orderId: " + orderId, e);
+            throw new CommonApiException(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    new Error("failed-to-get-refund-payment", "failed to get refund payment with order id [" + orderId + "]")
+            );
+        }
+    }
+
+}
