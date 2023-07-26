d222afb08c2872aef62aa23f276a21db9690b4e1
Jarkko Laukkanen <jarkko.laukkanen@ambientia.fi>
2023-07-26 09:52:27 +0300

    [KYV-906] messageapi fix

--- messageapi/src/main/java/fi/hel/verkkokauppa/message/MessageApiApplication.java
+++ messageapi/src/main/java/fi/hel/verkkokauppa/message/MessageApiApplication.java
@@ -2,21 +2,21 @@ package fi.hel.verkkokauppa.message;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jms.JmsAutoConfiguration;
-import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {
-		JmsAutoConfiguration.class,
-		ActiveMQAutoConfiguration.class,
-})
+@SpringBootApplication
 @ComponentScan({
-		"fi.hel.verkkokauppa.common.queue"
+        "fi.hel.verkkokauppa.message",
+        "fi.hel.verkkokauppa.common.configuration",
+        "fi.hel.verkkokauppa.common.queue.config",
+        "fi.hel.verkkokauppa.common.queue.error",
+        "fi.hel.verkkokauppa.common.queue.service",
+        "fi.hel.verkkokauppa.common.queue",
 })
 public class MessageApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MessageApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MessageApiApplication.class, args);
+    }
 
 }
