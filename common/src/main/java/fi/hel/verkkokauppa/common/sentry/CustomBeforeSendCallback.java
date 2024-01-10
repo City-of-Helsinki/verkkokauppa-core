dfda6947449131915282edc4144d0d721f134c0a
Waltteri Pakalén <waltteri.pakalen@ambientia.fi>
2024-01-10 09:11:02 +0200

    KYV-961: send every common api exception to sentry

--- common/src/main/java/fi/hel/verkkokauppa/common/sentry/CustomBeforeSendCallback.java
+++ common/src/main/java/fi/hel/verkkokauppa/common/sentry/CustomBeforeSendCallback.java
@@ -2,6 +2,7 @@ package fi.hel.verkkokauppa.common.sentry;
 
 import fi.hel.verkkokauppa.common.error.CommonApiException;
 import io.sentry.SentryEvent;
+import io.sentry.SentryLevel;
 import io.sentry.SentryOptions;
 import io.sentry.Hint;
 import org.springframework.stereotype.Component;
@@ -11,8 +12,7 @@ public class CustomBeforeSendCallback implements SentryOptions.BeforeSendCallbac
     public SentryEvent execute(SentryEvent event, Hint hint) {
         if (event.getThrowable() instanceof CommonApiException) {
             if (!((CommonApiException) event.getThrowable()).getStatus().is5xxServerError()) {
-                // event.setLevel(SentryLevel.INFO);
-                return null;
+                event.setLevel(SentryLevel.INFO);
             }
             ((CommonApiException) event.getThrowable()).getErrors().getErrors().forEach(error -> event.addBreadcrumb(String.format("code: %s\nmessage: %s", error.getCode(), error.getMessage())));
         }
