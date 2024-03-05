60c9071bad87a55d878dfb4bc64bd458527824d0
Waltteri Pakalén <waltteri.pakalen@ambientia.fi>
2024-03-05 16:14:33 +0200

    KYV-1034: implement resource locking

--- orderapi/src/main/java/fi/hel/verkkokauppa/order/api/admin/OrderAdminController.java
+++ orderapi/src/main/java/fi/hel/verkkokauppa/order/api/admin/OrderAdminController.java
@@ -2,6 +2,7 @@ package fi.hel.verkkokauppa.order.api.admin;
 
 import fi.hel.verkkokauppa.common.error.CommonApiException;
 import fi.hel.verkkokauppa.common.error.Error;
+import fi.hel.verkkokauppa.common.lock.ResourceLock;
 import fi.hel.verkkokauppa.order.api.data.OrderAggregateDto;
 import fi.hel.verkkokauppa.order.model.Order;
 import fi.hel.verkkokauppa.order.service.order.OrderService;
@@ -11,9 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +24,9 @@ public class OrderAdminController {
 
     @Autowired
     private OrderService orderService;
+
+    @Autowired
+    private ResourceLock resourceLock;
 
     // when returning from payment, userId is not available at client
     @GetMapping(value = "/order-admin/get", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -41,6 +43,35 @@ public class OrderAdminController {
             );
         }
     }
+
+    @PostMapping(value = "/order-admin/lock", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Boolean> lockOrder(@RequestParam(value = "orderId") String orderId) {
+        try {
+            boolean res = resourceLock.obtain(orderId);
+            return ResponseEntity.ok().body(res);
+        } catch (Exception e) {
+            log.error("locking order failed, orderId: " + orderId, e);
+            throw new CommonApiException(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    new Error("failed-to-lock-order", "failed to lock order with id [" + orderId + "]")
+            );
+        }
+    }
+
+    @PostMapping(value = "/order-admin/unlock", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> unlockOrder(@RequestParam(value = "orderId") String orderId) {
+        try {
+            resourceLock.release(orderId);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            log.error("unlocking order failed, orderId: " + orderId, e);
+            throw new CommonApiException(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    new Error("failed-to-unlock-order", "failed to unlock order with id [" + orderId + "]")
+            );
+        }
+    }
+
 
     @GetMapping(value = "/order-admin/get-all", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<OrderAggregateDto>> getOrders(@RequestParam(value = "userId") String userId) {
