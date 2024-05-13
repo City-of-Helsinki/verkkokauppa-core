c3c490049000b133e8292a21ef4897a4a6375b99
severikupari1 <severikupari1@gmail.com>
2024-05-13 12:12:29 +0300

    [KYV-1054] Use internal database using configuration

---
+++ productapi/src/main/java/fi/hel/verkkokauppa/product/configuration/ElasticsearchRestClientConfig.java
@@ -0,0 +1,24 @@
+package fi.hel.verkkokauppa.product.configuration;
+
+import fi.hel.verkkokauppa.common.elastic.ElasticSearchRestClientResolver;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+
+@Configuration
+public class ElasticsearchRestClientConfig extends AbstractElasticsearchConfiguration {
+
+    private ElasticSearchRestClientResolver elasticSearchRestClientResolver;
+
+    public ElasticsearchRestClientConfig(@Autowired ElasticSearchRestClientResolver elasticResolver) {
+        this.elasticSearchRestClientResolver = elasticResolver;
+    }
+
+    @Override
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+       return this.elasticSearchRestClientResolver.get();
+    }
+}
